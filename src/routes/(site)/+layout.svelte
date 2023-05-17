0c79ac266e5a7987c6abfa44e2cdb425b0eb946c
Alessio Grespi <56415370+AlessioGrespi@users.noreply.github.com>
2023-05-18 00:47:38 +0100

    scaffolding

---
+++ src/routes/(site)/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Footer from "../../lib/components/site/Footer.svelte";
+
+
+	import Nav from "../../lib/components/site/Nav.svelte";
+
+</script>
+<main>
+    <Nav />
+    <slot/>
+    <Footer />
+</main>
+
